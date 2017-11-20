983d719e1c9b56c47a4760e46b1a76bac2f0cfcb
Jabri <donjabricio@gmail.com>
2017-11-20 09:18:22 +0100

    Step 2

---
+++ server/datasources.local.js
@@ -0,0 +1,32 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+var config = require('./config.local.js')
+
+module.exports = {
+  DbLista: {
+    name: "DbLista",
+    connector: 'mysql',
+    hostname: config.db_host,
+    port: config.db_port,
+    user: config.db_user,
+    password: config.db_password,
+    database: 'DBListaCompra',
+  }/*,
+  
+  myEmailDataSource: {
+    name: "myEmailDataSource",
+    connector: "mail",
+    transports: [{
+      type: "SMTP",
+      host: "smtp.gmail.com",
+      secure: true,
+      port: 465,
+      auth: {
+        user: config.email_user,
+        pass: config.email_password
+      }
+    }]
+  }*/
+};
