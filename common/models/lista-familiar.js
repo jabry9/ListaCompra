5264e1dc8f19b4b3f0108a38350217fe0cad4010
Jabri <donjabricio@gmail.com>
2017-11-20 10:49:47 +0100

    Asignar lista a usuario y viceversa

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
-
+module.exports = function (Listafamiliar) {
+    Listafamiliar.beforeRemote('create', function (context, listaFamiliar, next) {
+        context.args.data.Owner = context.req.accessToken.userId;
+        next();
+    });
+        Listafamiliar.afterRemote('create', function (context, listaFamiliar, next) {
+        //context.args.data.Owner = context.req.accessToken.userId;
+        var app = Listafamiliar.app;
+        var Usuario = app.models.Usuario;
+        var IdUser = context.req.accessToken.userId;
+        
+        Usuario.findById(IdUser, function (err, usr) {
+            if (err)
+                return cb(err);
+            usr.PerteneceId = listaFamiliar.id;
+            usr.save();
+            next();
+        })
+    });
 };
